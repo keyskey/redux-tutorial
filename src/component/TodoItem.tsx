afd20821f15e78a8e405743c739c2fa4ee9d80c3
Keisuke Nagashima <energyrevo21@gmail.com>
2019-06-06 01:52:20 +0900

    Add checked property to TodoItem

--- src/component/TodoItem.tsx
+++ src/component/TodoItem.tsx
@@ -5,13 +5,16 @@ import ListItemText from '@material-ui/core/ListItemText';
 export interface TodoItemProps {
   title: string;
   id: number;
+  checked: boolean;
 }
 
 // ファイルの最後にTodoItemPropsとまとめてexportしようとすると他のモジュールで読み込めないので, ここだけはinterfaceや関数の宣言時にexportを付けておく
 export const TodoItem = (props: TodoItemProps) => {
+  const { title } = props;
+
   return (
     <ListItem button>
-      <ListItemText primary={props.title} />
+      <ListItemText primary={title} />
     </ListItem>
   );
 };
