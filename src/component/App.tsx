403f26a7a44d27e4d5e61a7a8b67115ed4b6c963
Keisuke Nagashima <energyrevo21@gmail.com>
2019-06-06 01:54:58 +0900

    Add CheckBoxList component and clearCheckedTodo method

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -4,6 +4,7 @@ import TodoInput from './TodoInput.tsx';
 import TodoItemProps from './TodoItem.tsx';
 import TodoList from './TodoList.tsx';
 import TodoCounter from './TodoCounter.tsx';
+import CheckBoxList from './CheckBoxList.tsx';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -30,13 +31,19 @@ class App extends React.Component<AppProps, AppState> {
   constructor(props) {
     super(props);
     this.state = {
-      tasks: [{ title: 'タスクその1', id: 0 }],
+      tasks: [
+        {
+          title: 'タスクその1',
+          id: 0,
+          checked: false
+        }
+      ],
       uniqueId: 1
     };
   }
 
   addTodo = (title: string): void => {
-    const newTask = { title: title, id: this.state.uniqueId };
+    const newTask = { title: title, id: this.state.uniqueId, checked: false };
     const newTasks = this.state.tasks.concat(newTask);
 
     this.setState({ tasks: newTasks, uniqueId: this.state.uniqueId + 1 });
@@ -44,6 +51,30 @@ class App extends React.Component<AppProps, AppState> {
 
   clearTodo = (): void => {
     this.setState({ tasks: [], uniqueId: 0 });
+  };
+
+  checkTodo = (taskId: number, checked: boolean): void => {
+    const checkedTodos = this.state.tasks.map(task => {
+      if (task.id === taskId) {
+        return Object.assign(task, { checked: checked });
+      } else {
+        return task;
+      }
+    });
+
+    this.setState({ tasks: checkedTodos });
+  };
+
+  clearCheckedTodo = (): void => {
+    const nonSelectedTodos = this.state.tasks.filter(
+      task => task.checked == false
+    );
+    const nextUniqueId = nonSelectedTodos.length;
+    const renumberedTodos = nonSelectedTodos.map((todo, index) =>
+      Object.assign(todo, { id: index })
+    ); // 選択したTodoの削除後はtask.idが連番になっていないので貼り直す
+
+    this.setState({ tasks: renumberedTodos, uniqueId: nextUniqueId });
   };
 
   render() {
@@ -63,11 +94,15 @@ class App extends React.Component<AppProps, AppState> {
         </AppBar>
         <Grid container>
           <Grid item xs={6}>
-            <TodoInput addTodo={this.addTodo} clearTodo={this.clearTodo} />
-            <TodoList tasks={tasks} />
+            <TodoInput
+              addTodo={this.addTodo}
+              clearCheckedTodo={this.clearCheckedTodo}
+            />
+            <CheckBoxList tasks={tasks} checkTodo={this.checkTodo} />
           </Grid>
           <Grid item xs={4}>
             <TodoCounter count={tasks.length} />
+            <TodoList tasks={tasks} />
           </Grid>
         </Grid>
       </div>
---
+++ src/component/CheckBoxList.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { TodoItemProps } from './TodoItem.tsx';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox, { CheckboxProps } from '@material-ui/core/Checkbox';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  checkBoxList: {
+    paddingLeft: 15
+  }
+};
+
+interface CheckBoxListProps {
+  tasks: TodoItemProps[];
+  classes: any;
+  checkTodo: (taskId: number, checked: boolean) => void;
+}
+
+const CheckBoxList = (props: CheckBoxListProps) => {
+  const { tasks, classes, checkTodo } = props;
+
+  const handleChange = (taskId: number) => (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    checkTodo(taskId, event.target.checked);
+  };
+
+  const checkboxes = tasks.map(task => {
+    return (
+      <FormControlLabel
+        control={
+          <Checkbox checked={task.checked} onChange={handleChange(task.id)} />
+        }
+        label={task.title}
+        key={task.id}
+      />
+    );
+  });
+
+  return <FormGroup className={classes.checkBoxList}>{checkboxes}</FormGroup>;
+};
+
+export default withStyles(styles)(CheckBoxList);
