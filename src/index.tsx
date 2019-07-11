56d0d8f2524af405b72a7a1b44d7992f53151e1f
Keisuke Nagashima <energyrevo21@gmail.com>
2019-07-11 16:22:04 +0900

    Set store to root component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './component/App';
+import tasksReducers, { initialState } from './reducers/tasks';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(tasksReducers, initialState);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root') as HTMLElement
+);
+
 serviceWorker.unregister();
