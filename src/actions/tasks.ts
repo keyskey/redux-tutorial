e5de56b92364cc84bf4beb231357df03bfe166de
Keisuke Nagashima <energyrevo21@gmail.com>
2019-07-11 16:18:36 +0900

    Add actions

---
+++ src/actions/tasks.ts
@@ -0,0 +1,41 @@
+export enum TaskActionTypes {
+  ADD_TASK = 'ADD_TASK',
+  INPUT_TASK = 'INPUT_TASK',
+  CHECK_TASK = 'CHECK_TASK',
+  CLEAR_CHECKED_TASK = 'CLEAR_CHECKED_TASK'
+}
+
+export interface TaskAction {
+  type: TaskActionTypes;
+  payload?: {
+    content?: string;
+    taskId?: number;
+    checked?: boolean;
+  };
+}
+
+export const addTask = (content: string): TaskAction => ({
+  type: TaskActionTypes.ADD_TASK,
+  payload: {
+    content: content
+  }
+});
+
+export const inputTask = (content: string): TaskAction => ({
+  type: TaskActionTypes.INPUT_TASK,
+  payload: {
+    content: content
+  }
+});
+
+export const checkTask = (taskId: number, checked: boolean): TaskAction => ({
+  type: TaskActionTypes.CHECK_TASK,
+  payload: {
+    taskId: taskId,
+    checked: checked
+  }
+});
+
+export const clearCheckedTask = (): TaskAction => ({
+  type: TaskActionTypes.CLEAR_CHECKED_TASK
+});
