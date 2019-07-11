bde723bfc80fdd56188778c763aec018ef36dc06
Keisuke Nagashima <energyrevo21@gmail.com>
2019-07-11 16:19:56 +0900

    Add reducer

---
+++ src/reducers/tasks.ts
@@ -0,0 +1,74 @@
+import { Reducer } from 'redux';
+import { TaskAction, TaskActionTypes } from '../actions/tasks';
+
+export interface Task {
+  content: string;
+  checked: boolean;
+}
+
+export interface TaskState {
+  inputValue: string;
+  tasks: Task[];
+}
+
+export const initialState: TaskState = {
+  inputValue: '',
+  tasks: [{ content: 'タスクその1', checked: false }]
+};
+
+const tasksReducer: Reducer<TaskState, TaskAction> = (
+  state = initialState,
+  action
+) => {
+  switch (action.type) {
+    case TaskActionTypes.ADD_TASK: {
+      if (action.payload.content === '') {
+        return state;
+      }
+
+      const newTask = {
+        content: action.payload.content,
+        checked: false
+      };
+
+      return {
+        ...state,
+        inputValue: '',
+        tasks: state.tasks.concat(newTask)
+      };
+    }
+
+    case TaskActionTypes.INPUT_TASK: {
+      return {
+        ...state,
+        inputValue: action.payload.content
+      };
+    }
+
+    case TaskActionTypes.CHECK_TASK: {
+      let tasks = [...state.tasks];
+      tasks[action.payload.taskId].checked = action.payload.checked;
+
+      return {
+        ...state,
+        tasks: tasks
+      };
+    }
+
+    case TaskActionTypes.CLEAR_CHECKED_TASK: {
+      const nonSelectedTasks = state.tasks.filter(
+        (task, i) => task.checked === false
+      );
+
+      return {
+        ...state,
+        tasks: nonSelectedTasks
+      };
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default tasksReducer;
